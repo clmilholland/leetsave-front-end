0667fc52db13e163dcd9bee5c82063be14b5a03b
clmilholland <luke12milholland@gmail.com>
2025-08-06 19:26:44 -0600

    Made small UI/UX changes to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,21 +2,40 @@ import React from 'react';
 import styles from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 import LeetSaveLogo from '../../images/LeetSaveLogo.png'
-
+import { FaRegUser, FaRegHeart } from "react-icons/fa";
+import { GoHome, GoCodeReview } from "react-icons/go";
+import { SiBookstack } from "react-icons/si";
+import { useState } from 'react';
 
 const Header = () => {
+  const [active, setActive] = useState('Home');
+  console.log(active)
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
+      <NavLink to={"/"} className={styles.logoContainer} >
         <img src={LeetSaveLogo} alt="LeetSave Logo" className={styles.logo} />
-      </div>
+      </NavLink>
       <nav className={styles.nav}>
-        <NavLink to="/" className={styles.navItem}>Home</NavLink>
-        <NavLink to="/problems" className={styles.navItem}>Problems</NavLink>
-        <NavLink to="/problems/favorites" className={styles.navItem}>Favorites</NavLink>
-        <NavLink to="/create" className={styles.navItem}>Create</NavLink>
-        <NavLink to="/study" className={styles.navItem}>Study</NavLink>
-        <NavLink to="/profile" className={styles.navItem}>Profile</NavLink>
+        <NavLink className={`${styles.navItemContainer} ${active === 'Home' ? styles.active : styles.inactive}`} onClick={() => setActive('Home')}>
+          <GoHome className={styles.icon} />
+          Home
+        </NavLink>
+        <NavLink to="/problems" className={`${styles.navItemContainer} ${active === 'Problems' ? styles.active : styles.inactive}`} onClick={() => setActive('Problems')}>
+          <GoCodeReview className={styles.icon} />
+          Problems
+        </NavLink>
+        <NavLink to="/problems/favorites" className={`${styles.navItemContainer} ${active === 'Favorites' ? styles.active : styles.inactive}`} onClick={() => setActive('Favorites')}>
+          <FaRegHeart className={styles.icon} />
+          Favorites
+        </NavLink>
+        <NavLink to="/study" className={`${styles.navItemContainer} ${active === 'Study' ? styles.active : styles.inactive}`} onClick={() => setActive('Study')}>
+          <SiBookstack className={styles.icon} />
+          Study
+        </NavLink>
+        <NavLink to="/profile" className={`${styles.navItemContainer} ${active === 'Profile' ? styles.active : styles.inactive}`} onClick={() => setActive('Profile')}>
+          <FaRegUser className={styles.icon} />
+          Profile
+        </NavLink>
       </nav>
     </header>
   );
